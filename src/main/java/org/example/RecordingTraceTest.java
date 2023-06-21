f8ab586e3214599307f68a5e8819d0bd8318c834
Sarthak Giri <giri.sarthak1@gmail.com>
2023-06-21 13:15:17 +0530

    Removing unused imports

--- src/main/java/org/example/RecordingTraceTest.java
+++ src/main/java/org/example/RecordingTraceTest.java
@@ -2,10 +2,7 @@ package org.example;
 
 import com.microsoft.playwright.*;
 import com.microsoft.playwright.options.AriaRole;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.nio.file.Paths;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
